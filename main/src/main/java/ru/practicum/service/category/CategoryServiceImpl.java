da91b0f3fc840f58080daaf1c22d9b6facce9ba9
Emizene <elmira.zenkova@yandex.ru>
2025-10-01 20:07:01 +0300

    Исправления

--- main/src/main/java/ru/practicum/service/category/CategoryServiceImpl.java
+++ main/src/main/java/ru/practicum/service/category/CategoryServiceImpl.java
@@ -7,13 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.practicum.categoryDto.CategoryDto;
 import ru.practicum.categoryDto.NewCategoryDto;
-import ru.practicum.exception.AlreadyExistsException;
 import ru.practicum.exception.ConflictException;
 import ru.practicum.exception.NotFoundException;
 import ru.practicum.mapper.CategoryMapper;
 import ru.practicum.model.Category;
 import ru.practicum.repository.CategoryRepository;
-import ru.practicum.repository.EventRepository;
 
 import java.util.List;
 
@@ -24,7 +22,6 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
-    private final EventRepository eventRepository;
 
     @Override
     public CategoryDto createCategory(NewCategoryDto newCategoryDto) {
